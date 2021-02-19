4a7f71a0b13362e79bdb40972ec927bdcc3c646c
Kevin Nelson <nelson67@stolaf.edu>
2021-02-18 19:41:52 -0600

    all

---
+++ content.js
@@ -0,0 +1,13 @@
+var main = document.getElementById("main")
+var editArea = document.getElementById("edit_area")
+
+main.appendChild(editArea)
+
+document.getElementById("prompt_outer").style.flex = "none"
+document.getElementById("prompt_outer").style.width = "33vw"
+
+document.getElementById("workspace").style.flex = "none"
+document.getElementById("workspace").style.width = "33vw"
+
+document.getElementById("edit_area").style.flex = "none"
+document.getElementById("edit_area").style.width = "33vw"
